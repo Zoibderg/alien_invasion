39b4b1022f4868832e6f7b6fa3d071e62a0263ad
Zoibderg <43228316+Zoibderg@users.noreply.github.com>
2021-11-05 13:18:34 -0700

    added continuous right movement to our ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,6 +29,8 @@ class AlienInvasion:
         while True:
             #Watch for keyboard and mouse events
             self._check_events()
+            # draw the ship to the screen
+            self.ship.update()
             #call to update images on the screen
             self._update_screen()
 
@@ -39,6 +41,18 @@ class AlienInvasion:
             #if player preses close, quit game
             if event.type == pygame.QUIT:
                 sys.exit()
+            # if event is a key press
+            elif event.type == pygame.KEYDOWN:
+                #if that key press is right arrow
+                if event.key == pygame.K_RIGHT:
+                    # set movement flag to True
+                    self.ship.moving_right = True
+            # if event is releasing a key
+            elif event.type == pygame.KEYUP:
+                # if that released key is right arrow key
+                if event.key == pygame.K_RIGHT:
+                    # set the movement flag to False
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Update images on screen and flip to the new screen."""
--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
     def __init__(self):
         """Initlize the games settings."""
         #All screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1600
+        self.screen_height = 900
         #this is our background color, which is a light gray
         self.bg_color = (230, 230, 230)
