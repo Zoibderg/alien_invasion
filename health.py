4f2e877f015711fa7b6bb06901374d38312c2349
Zoibderg <43228316+Zoibderg@users.noreply.github.com>
2021-11-14 12:54:44 -0800

    added empty heart sprites for a smoother UI

--- health.py
+++ health.py
@@ -2,10 +2,9 @@ import pygame
 from pygame.sprite import Sprite
 
 class Health(Sprite):
-    """A class for managing our ship."""
+    """A class for our full hearts."""
 
     def __init__(self, ai_game):
-        """Initlize the ship and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
@@ -16,5 +15,20 @@ class Health(Sprite):
         self.rect = self.image.get_rect()
 
     def blitme(self):
-        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+
+class EmptyHealth(Sprite):
+    """a class for out empty hearts"""
+
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('images/empty_heart.bmp')
+        self.rect = self.image.get_rect()
+
+    def blitme(self):
         self.screen.blit(self.image, self.rect)
--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,7 @@
 import pygame.font
 
 from pygame.sprite import Group
-from health import Health
+from health import EmptyHealth, Health
 from ship import Ship
 
 class Scoreboard:
@@ -21,6 +21,7 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_empty_hearts()
         self.prep_ships()
 
     def prep_score(self):
@@ -67,6 +68,14 @@ class Scoreboard:
             ship.rect.y = 10
             self.ships.add(ship)
 
+    def prep_empty_hearts(self):
+        self.empty_hearts = Group()
+        for ship_number in range(self.settings.ship_limit):
+            empty_heart = EmptyHealth(self.ai_game)
+            empty_heart.rect.x = 10 + ship_number * empty_heart.rect.width
+            empty_heart.rect.y = 10
+            self.empty_hearts.add(empty_heart)
+
     def check_high_score(self):
         """check for a new high score"""
         if self.stats.score > self.stats.high_score:
@@ -78,4 +87,5 @@ class Scoreboard:
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.empty_hearts.draw(self.screen)
         self.ships.draw(self.screen)
